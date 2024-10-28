89adc905871e20b3f1c77b07da9ab0e4521c9287
d-otynshin <daniyar.otynshin@gmail.com>
2024-10-29 01:22:40 +0500

    remove mock, add logs to url mongo

--- src/db/monogo-db.ts
+++ src/db/monogo-db.ts
@@ -4,6 +4,8 @@ dotenv.config();
 
 const MONGO_DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
+console.log('MONGO_DB_URI', MONGO_DB_URI);
+
 const client: MongoClient = new MongoClient(MONGO_DB_URI)
 export const db: Db = client.db('blogger_platform');
 
