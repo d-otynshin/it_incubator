ac844d65bf4a0cce9aca72857d539a96641ab031
d-otynshin <daniyar.otynshin@gmail.com>
2024-10-29 01:57:54 +0500

    remove logs

--- src/db/monogo-db.ts
+++ src/db/monogo-db.ts
@@ -4,8 +4,6 @@ dotenv.config();
 
 const MONGO_DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
-console.log('MONGO_DB_URI', MONGO_DB_URI);
-
 const client: MongoClient = new MongoClient(MONGO_DB_URI)
 export const db: Db = client.db('blogger_platform');
 
