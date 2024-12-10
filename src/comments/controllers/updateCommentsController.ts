fb9c05be2f9d156744da02eea56a61cb200051ff
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-10 16:33:19 +0500

    add forbidden and not found errors

--- src/comments/controllers/updateCommentsController.ts
+++ src/comments/controllers/updateCommentsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { commentsService } from '../comments-service';
+import { commentsQueryRepository } from '../comments-query-repository';
 
 export const updateCommentsController = async (
   req: Request,
@@ -8,7 +9,20 @@ export const updateCommentsController = async (
   const { content } = req.body;
   const { id } = req.params;
 
+  //@ts-ignore
+  const { id: userId } = req.user;
+
+  const comment = await commentsQueryRepository.getById(id);
+
+  if (!comment) {
+    return res.status(404).send({ error: 'Comment not found' });
+  }
+
+  if (comment.commentatorInfo.userId !== userId) {
+    return res.status(403).send({ error: 'Comment update is forbidden' });
+  }
+
   const isUpdated = await commentsService.update({ id, content });
 
-  return isUpdated ? res.status(204).send() : res.status(400).json();
+  return isUpdated ? res.status(204).send() : res.status(400).json({ message: 'Comment was not updated' });
 }
