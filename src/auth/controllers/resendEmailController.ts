f1060fc1d0067915fccd7e7de8a38dec6c7e3e5c
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-12 23:40:33 +0500

    fix resend email controller

--- src/auth/controllers/resendEmailController.ts
+++ src/auth/controllers/resendEmailController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 import { authService } from '../auth-service';
 
-export const resendEmailController = async (req: Request, res: Response) => {
+export const resendEmailController = async (
+  req: Request,
+  res: Response
+) => {
   const { email } = req.body;
 
   const isResent = await authService.resendEmail(email);
