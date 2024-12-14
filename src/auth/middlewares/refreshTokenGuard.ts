3e563e08db84bf909b78ff1d0bbb2fb9c44d3d78
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-15 00:04:52 +0500

    add refresh token guard try catch wrap

--- src/auth/middlewares/refreshTokenGuard.ts
+++ src/auth/middlewares/refreshTokenGuard.ts
@@ -7,20 +7,24 @@ export const refreshTokenGuard = async (
   response: Response,
   next: NextFunction
 ) => {
-  const refreshToken = request.cookies.refreshToken;
-  if (!refreshToken) {
-    return response.status(401).json({ error: 'No refresh token provided' });
-  }
+  try {
+    const refreshToken = request.cookies.refreshToken;
+    if (!refreshToken) {
+      return response.status(401).json({ error: 'No refresh token provided' });
+    }
 
-  const isTokenExpired = await jwtService.isExpired(refreshToken, 'REFRESH');
-  if (isTokenExpired) {
-    return response.status(401).json({ error: 'Refresh token expired' });
-  }
+    const isTokenExpired = await jwtService.isExpired(refreshToken, 'REFRESH');
+    if (isTokenExpired) {
+      return response.status(401).json({ error: 'Refresh token expired' });
+    }
 
-  const isTokenInvalid = await authRepository.getInvalidToken(refreshToken);
-  if (isTokenInvalid) {
+    const isTokenInvalid = await authRepository.getInvalidToken(refreshToken);
+    if (isTokenInvalid) {
+      return response.status(401).json({ error: 'Refresh token invalid' });
+    }
+
+    return next()
+  } catch (error) {
     return response.status(401).json({ error: 'Refresh token invalid' });
   }
-
-  return next()
 }
