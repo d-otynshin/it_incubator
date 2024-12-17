b49f3008cddb65430a578640069fe0c62dab9beb
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-17 18:24:28 +0500

    fix mapSession

--- src/security/helpers/mapSession.ts
+++ src/security/helpers/mapSession.ts
@@ -12,7 +12,7 @@ export const mapSession = (sessionDB: TSessionDb) => {
   return ({
     deviceId: sessionDB.deviceId,
     ip: sessionDB.ip,
-    lastActiveDate: new Date(sessionDB.iat).toString(),
+    lastActiveDate: new Date(sessionDB.iat * 1000).toISOString(),
     title: sessionDB.name,
   })
 }
