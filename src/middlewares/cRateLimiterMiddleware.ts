e17534d2349dce112f0484834058f35a127fa393
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-18 12:29:52 +0500

    fix rate limiter

--- src/middlewares/cRateLimiterMiddleware.ts
+++ src/middlewares/cRateLimiterMiddleware.ts
@@ -15,14 +15,15 @@ export const cRateLimiterMiddleware = async (
       log => isAfter(log.date, subSeconds(new Date(), 100))
     );
 
-    if (filteredLogs.length > 5) {
+    await crateLimiterRepository.set(ip as string, url, new Date());
+
+    if (filteredLogs.length >= 5) {
       return response.status(429).json({ message: 'Rate limit' })
     }
 
-    await crateLimiterRepository.set(ip as string, url, new Date());
 
     return next()
   } catch (error) {
-    return response.status(401).json({ error: '' });
+    return response.status(401).json({ message: 'cRateLimiterMiddleware' });
   }
 }
