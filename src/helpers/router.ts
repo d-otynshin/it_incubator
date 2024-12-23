0cea91ab8541d239b70a9151286952063625a889
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-23 18:23:19 +0500

    display mongo connection ready state

--- src/helpers/router.ts
+++ src/helpers/router.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 
-export const router = (
+export const router = async (
   _: Request,
   res: Response
 ) => {
+  const connection = await mongoose.connect(process.env.MONGODB_URI as string);
   const mongoStatus = mongoose.connection.readyState
-  res.status(200).json({ "connection": mongoStatus })
+  res.status(200).json({ "connection": mongoStatus, "response": connection })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { app } from './app'
 import { SETTINGS } from './settings'
-import { connectToDb } from './db';
+// import { connectToDb } from './db';
 
 app.listen(SETTINGS.PORT, async () => {
-  await connectToDb()
+  // await connectToDb()
   console.log('...server started in port ' + SETTINGS.PORT)
 })
 
