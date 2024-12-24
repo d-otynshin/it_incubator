b26887f778f133400842f4d9fd5fa9c1f88955ed
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-24 15:02:38 +0500

    remove catch

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,11 +5,6 @@ dotenv.config();
 const MONGO_DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 export const connectToDb = async (connectionUrl: string): Promise<void> => {
-  try {
-    await mongoose.connect(connectionUrl);
-    console.log('connected to db')
-  } catch (e) {
-    console.log('db connection error:', e)
-    await mongoose.disconnect();
-  }
+  await mongoose.connect(connectionUrl);
+  console.log('connected to db')
 }
