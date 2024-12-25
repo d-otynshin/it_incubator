44f9c1d78c97e06fe9efefb8c08cbc055fc12080
d-otynshin <daniyar.otynshin@gmail.com>
2024-12-25 16:43:35 +0500

    add settings port log

--- src/helpers/router.ts
+++ src/helpers/router.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { connectToDb } from '../db';
+import { SETTINGS } from '../settings';
 
 export const router = async (
   _: Request,
   res: Response
 ) => {
   await connectToDb()
+
+  console.log('settings port', SETTINGS.PORT)
 
   const mongoStatus = mongoose.connection.readyState
   res.status(200).json({ "connection": mongoStatus })
